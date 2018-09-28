d50c0c03b8773d2453d05bedab7364bc5fbbc472
angelfffjjj <43463617+angelfffjjj@users.noreply.github.com>
2018-09-28 16:08:44 +0800

    Add files via upload

---
+++ exercise/2018928 class.R
@@ -0,0 +1,40 @@
+my_num1 <- 2.33
+my_num1
+my_num2 <- 2.0
+my_num2
+my_num3 <- 2
+my_num3
+
+my_int1 <- 2L
+my_int1
+my_int2 <- 2.0L
+my_int2
+my_int3 <- 2.33L
+my_int3
+
+TRUE
+FALSE
+T
+F
+True
+true
+
+8 > 7 # 耞 8 琌??? 7
+8 < 7 # 耞 8 琌??? 7
+8 >= 7 # 耞 8 琌???单 7
+8 <= 7 # 耞 8 琌???单 7
+8 == 7 # 耞 8 琌单 7
+8 != 7 # 耞 8 琌ぃ单 7
+7 %in% c(8, 7) # 耞 7 琌??? c(8, 7) ぇい
+
+first_name <- "Tony"
+first_name
+class(first_name)
+
+sys_date <- Sys.Date() # ╰参???戳
+sys_date # 癬ㄓ蛤???
+class(sys_date)
+
+sys_time <- Sys.time() # ╰参丁
+sys_time # 癬ㄓ蛤???
+class(sys_time)
